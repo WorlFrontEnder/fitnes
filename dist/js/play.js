1c275e83ef39a8d6a8ad978fbaa166d654847b5b
WorlFrontEnder <Cyberverstalshik@yandex.ru>
2018-07-13 07:55:44 +0400

    Ссылки на видео контент

--- dist/js/play.js
+++ dist/js/play.js
@@ -1,10 +1,10 @@
 function play() {
  $('.video__youtube').hide();
-    document.getElementById('video-wrap').innerHTML = '<iframe src="https://www.youtube.com/embed/bCi6TzRoAHc?autoplay=1&rel=0&showinfo=0" height="287" width="478" frameborder="0" allowfullscreen></iframe>';
+    document.getElementById('video-wrap').innerHTML = '<iframe width="478" height="287" src="https://www.youtube.com/embed/l5skUW3u1zw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
 }
 
 
 function play2() {
     $('.video__youtube2').hide();
-       document.getElementById('video-wrap2').innerHTML = '<iframe src="https://www.youtube.com/embed/bCi6TzRoAHc?autoplay=1&rel=0&showinfo=0" height="287" width="478" frameborder="0" allowfullscreen></iframe>';
+       document.getElementById('video-wrap2').innerHTML = '<iframe width="478" height="287" src="https://www.youtube.com/embed/BH86Ty4Am2c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
    }
