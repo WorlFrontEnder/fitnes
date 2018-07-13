6da389de2053b9b22a968a90fdc92aa61bec7c3e
WorlFrontEnder <Cyberverstalshik@yandex.ru>
2018-07-13 07:36:29 +0400

    Инициализация проекта

---
+++ dist/js/play.js
@@ -0,0 +1,10 @@
+function play() {
+ $('.video__youtube').hide();
+    document.getElementById('video-wrap').innerHTML = '<iframe src="https://www.youtube.com/embed/bCi6TzRoAHc?autoplay=1&rel=0&showinfo=0" height="287" width="478" frameborder="0" allowfullscreen></iframe>';
+}
+
+
+function play2() {
+    $('.video__youtube2').hide();
+       document.getElementById('video-wrap2').innerHTML = '<iframe src="https://www.youtube.com/embed/bCi6TzRoAHc?autoplay=1&rel=0&showinfo=0" height="287" width="478" frameborder="0" allowfullscreen></iframe>';
+   }
